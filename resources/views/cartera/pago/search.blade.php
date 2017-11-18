4be9786e83cce7ec0fba479a948f8244b6d627b4
WilsonArce <wdarcedev@gmail.com>
2017-11-18 13:16:00 -0500

    Cambio del tipo de dato que recibe el input
    Cambiándolo por tipo 'number' no permite ingresar otros caracteres que no sean numeros

--- resources/views/cartera/pago/search.blade.php
+++ resources/views/cartera/pago/search.blade.php
@@ -1,7 +1,9 @@
 {!! Form::open(array('url'=>'pago', 'method'=>'GET', 'autocomplete'=>'off', 'role'=>'search'))!!}
 <div class="form-group">
 	<div class="input-group">
-		<input type="text" class="form-control" name="searchText" placeholder="Buscar...." value="{{$searchText}}">
+		<!-- cambiar el tipo de input por number para así evitar ingreso de letras, espacios o caracteres especiales -->
+		<input type="number" class="form-control" name="searchText" placeholder="Buscar...." value="{{$searchText}}">
+		<!--input type="text" class="form-control" name="searchText" placeholder="Buscar...." value="{{$searchText}}"-->
 		<span class="input-group-btn">
 		<button type="submit" class="btn btn-primary">Buscar</button>
 			
